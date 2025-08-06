994117e63f29035c1bd9e51c1a0aa773b9196abc
kashi992 <kashifrajput992@gmail.com>
2025-08-06 20:40:01 +0500

    arrows done

--- src/pages/home-page/heroSlider/index.jsx
+++ src/pages/home-page/heroSlider/index.jsx
@@ -8,21 +8,25 @@ import "swiper/css/navigation"; // Import Swiper navigation styles
 import { EffectCoverflow, Pagination, Navigation } from "swiper/modules";
 import "./index.css"; // For custom styles
 import { useNavigate, useLocation } from "react-router-dom";
+import swiperPrev from "../../../assets/images/leftChev.svg";
+import swiperNext from "../../../assets/images/rightChev.svg";
 const HeroSlider = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const swiperRef = useRef(null);
+   const prevRef  = useRef(null);
+  const nextRef  = useRef(null);
   const slides = [
     {
       id: 1,
-         image: "https://cpb-uglsolution-videos.s3-accelerate.amazonaws.com/acciona/theVision.png",
+      image: "https://cpb-uglsolution-videos.s3-accelerate.amazonaws.com/acciona/theVision.png",
       imageR: "https://cpb-uglsolution-videos.s3-accelerate.amazonaws.com/acciona/theVision.png",
       title: "The Vision",
       path: "/vision",
     },
     {
       id: 2,
-         image: "https://cpb-uglsolution-videos.s3-accelerate.amazonaws.com/acciona/flyThrough.png",
+      image: "https://cpb-uglsolution-videos.s3-accelerate.amazonaws.com/acciona/flyThrough.png",
       imageR: "https://cpb-uglsolution-videos.s3-accelerate.amazonaws.com/acciona/flyThrough.png",
       title: "Fly Through",
       path: "/fly-through",
@@ -36,14 +40,14 @@ const HeroSlider = () => {
     },
     {
       id: 4,
-    image: "https://cpb-uglsolution-videos.s3-accelerate.amazonaws.com/acciona/socialProcurement.png",
+      image: "https://cpb-uglsolution-videos.s3-accelerate.amazonaws.com/acciona/socialProcurement.png",
       imageR: "https://cpb-uglsolution-videos.s3-accelerate.amazonaws.com/acciona/socialProcurement.png",
       title: "Social Procurement",
       path: "/social-procurement",
     },
     {
       id: 5,
-    image: "https://cpb-uglsolution-videos.s3-accelerate.amazonaws.com/acciona/community.png",
+      image: "https://cpb-uglsolution-videos.s3-accelerate.amazonaws.com/acciona/community.png",
       imageR: "https://cpb-uglsolution-videos.s3-accelerate.amazonaws.com/acciona/community.png",
       title: "Community Engagement",
       path: "/community",
@@ -92,78 +96,92 @@ const HeroSlider = () => {
   return (
     <section className="heroWrap">
       <div className="container">
-        <Swiper
-          ref={swiperRef}
-          grabCursor={true}
-          centeredSlides={false}
-          slidesPerView={4}
-          slidesPerGroup={2}
-          spaceBetween={80}
-          loop={false}
-          pagination={true}
-          navigation
-          modules={[EffectCoverflow, Pagination, Navigation]}
-          className="heroSwiper"
-          breakpoints={{
-            // when window width is <= 499px
-            0: {
-              slidesPerView: 1,
-              spaceBetween: 30,
-              centeredSlides: false,
-              slidesPerGroup: 1,
-            },
-            575: {
-              slidesPerView: 3,
-              spaceBetween: 50,
-              centeredSlides: false,
-            },
-            // when window width is <= 999px
-            999: {
-              slidesPerView: 3,
-              spaceBetween: 60,
-              centeredSlides: false,
-            },
-            // when window width is <= 999px
-            1200: {
-              spaceBetween: 80,
-              slidesPerGroup: 3,
-            },
-            1680: {
-              slidesPerView: 4,
-              spaceBetween: 80,
-              slidesPerGroup: 3,
-            },
-            1920: {
-              slidesPerView: 4,
-              spaceBetween: 80,
-              slidesPerGroup: 3,
-            },
-          }}
-        >
-          {slides.map((slide, index) => (
-            <SwiperSlide
-              key={slide.id}
-              // onClick={() => handleSlideClick(slide)} // Open thumbnails view
-              onClick={() =>
-                navigate(slide.path, { state: { fromSlideIndex: index } })
-              }
-            >
-              <div className="sliderContent">
-                <div className="slideImg">
-                  <img className="slideImgN" src={slide.image} alt={`Slide ${slide.id}`} />
-                  <img className="slideImgR" src={slide.imageR} alt={`Slide ${slide.id}`} />
+        <div className="swiperContainer">
+          <button ref={prevRef} className="swiperPrev"><img src={swiperPrev} alt="" /></button>
+          <button ref={nextRef} className="swiperNext"><img src={swiperNext} alt="" /></button>
+          <Swiper
+            ref={swiperRef}
+            grabCursor={true}
+            centeredSlides={false}
+            slidesPerView={4}
+            slidesPerGroup={2}
+            spaceBetween={80}
+            loop={false}
+            pagination={true}
+           navigation={{
+              // these get overwritten by onBeforeInit
+              prevEl: prevRef.current,
+              nextEl: nextRef.current,
+            }}
+               onBeforeInit={(swiper) => {
+              // swap out the default selectors for your refs
+              swiper.params.navigation.prevEl = prevRef.current;
+              swiper.params.navigation.nextEl = nextRef.current;
+            }}
+            modules={[EffectCoverflow, Pagination, Navigation]}
+            className="heroSwiper"
+            breakpoints={{
+              // when window width is <= 499px
+              0: {
+                slidesPerView: 1,
+                spaceBetween: 30,
+                centeredSlides: false,
+                slidesPerGroup: 1,
+              },
+              575: {
+                slidesPerView: 3,
+                spaceBetween: 50,
+                centeredSlides: false,
+              },
+              // when window width is <= 999px
+              999: {
+                slidesPerView: 3,
+                spaceBetween: 60,
+                centeredSlides: false,
+              },
+              // when window width is <= 999px
+              1200: {
+                spaceBetween: 80,
+                slidesPerGroup: 3,
+              },
+              1680: {
+                slidesPerView: 4,
+                spaceBetween: 80,
+                slidesPerGroup: 3,
+              },
+              1920: {
+                slidesPerView: 4,
+                spaceBetween: 80,
+                slidesPerGroup: 3,
+              },
+            }}
+          >
+            {slides.map((slide, index) => (
+              <SwiperSlide
+                key={slide.id}
+                // onClick={() => handleSlideClick(slide)} // Open thumbnails view
+                onClick={() =>
+                  navigate(slide.path, { state: { fromSlideIndex: index } })
+                }
+              >
+                <div className="sliderContent">
+                  <div className="slideImg">
+                    <img className="slideImgN" src={slide.image} alt={`Slide ${slide.id}`} />
+                    <img className="slideImgR" src={slide.imageR} alt={`Slide ${slide.id}`} />
+                  </div>
+                  <h2 className="sf">{slide.title}</h2>
                 </div>
-                <h2 className="sf">{slide.title}</h2>
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+
         <h1>Interactive Bid Portal</h1>
         <div className="line1 absolute top-0 right-[-70px]">
           <img src="https://cpb-uglsolution-videos.s3-accelerate.amazonaws.com/acciona/line_03.png" alt="" className="min-[1600px]:w-[478px] min-[1570px]:w-[400px] min-[575px]:w-[350px] w-[260px]" />
         </div>
         <div className="line2 absolute right-0 bottom-0">
-          <img src="https://cpb-uglsolution-videos.s3-accelerate.amazonaws.com/acciona/line_04.png" alt="" className="min-[1570px]:w-[431px] min-[575px]:w-[300px] w-[135px]"/>
+          <img src="https://cpb-uglsolution-videos.s3-accelerate.amazonaws.com/acciona/line_04.png" alt="" className="min-[1570px]:w-[431px] min-[575px]:w-[300px] w-[135px]" />
         </div>
       </div>
     </section>
