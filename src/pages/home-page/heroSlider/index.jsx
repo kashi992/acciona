ab5340925ab08da13ccf8a1540ba33338716ba2a
kashi992 <kashifrajput992@gmail.com>
2025-08-07 16:20:18 +0500

    jnj

--- src/pages/home-page/heroSlider/index.jsx
+++ src/pages/home-page/heroSlider/index.jsx
@@ -94,7 +94,7 @@ const HeroSlider = () => {
   }, [location.state]);
 
   return (
-    <section className="heroWrap">
+    <section className="heroWrap" style={{backgroundColor:"red"}}>
       <div className="container">
         <div className="swiperContainer">
           <button ref={prevRef} className="swiperPrev"><img src={swiperPrev} alt="" /></button>
@@ -160,9 +160,9 @@ const HeroSlider = () => {
               <SwiperSlide
                 key={slide.id}
                 // onClick={() => handleSlideClick(slide)} // Open thumbnails view
-                onClick={() =>
-                  navigate(slide.path, { state: { fromSlideIndex: index } })
-                }
+                // onClick={() =>
+                //   navigate(slide.path, { state: { fromSlideIndex: index } })
+                // }
               >
                 <div className="sliderContent">
                   <div className="slideImg">
