28c71bf3a928275d310ed41a547307ca8f498630
kashi992 <kashifrajput992@gmail.com>
2025-10-30 11:10:08 +0500

    community icon changed

--- src/pages/home-page/heroSlider/index.jsx
+++ src/pages/home-page/heroSlider/index.jsx
@@ -178,7 +178,7 @@ const HeroSlider = () => {
                 // }
               >
                 <div className="sliderContent">
-                  <div className={`slideImg ${slide.disabled ? "greySlide" : ""} ${slide.id === 4 ? "SocialProcurement" : ""}`}>
+                  <div className={`slideImg ${slide.disabled ? "greySlide" : ""} ${slide.id === 5 ? "communityIcon" : ""}`}>
                     <img className="slideImgN" src={slide.image} alt={`Slide ${slide.id}`} />
                     <img className="slideImgR" src={slide.imageR} alt={`Slide ${slide.id}`} />
                   </div>
